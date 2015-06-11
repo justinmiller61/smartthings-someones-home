32c09de241e17735a99081cc005491411f86b4dd
Justin Miller <justin.adam.miller@gmail.com>
2015-06-11 17:52:59 -0400

    - added gitignore - removed ending newline

--- SomeonesHome.groovy
+++ SomeonesHome.groovy
@@ -32,96 +32,96 @@ definition(
 
 preferences {
 	page(name: "mainPage")
-    page(name: "timeIntervalInputStart")
-    page(name: "timeIntervalInputEnd")
-    page(name: "moreOptions")
+	page(name: "timeIntervalInputStart")
+	page(name: "timeIntervalInputEnd")
+	page(name: "moreOptions")
 }
 
 def mainPage() {
 
 	dynamicPage(name: "mainPage", title: "Status", install: true, uninstall: true) {
-        section("Which mode change triggers the simulator? (This app will only run in selected mode(s))") { 
-            input name: "newMode", type: "mode", title: "Which?", multiple: true, required: true 
-        }
+		section("Which mode change triggers the simulator? (This app will only run in selected mode(s))") {
+			input name: "newMode", type: "mode", title: "Which?", multiple: true, required: true
+		}
 
-        section("Light switches to turn on/off") { 
-            input name: "switches", type: "capability.switch", title: "Switches?", multiple: true, required: true 
-        }
+		section("Light switches to turn on/off") {
+			input name: "switches", type: "capability.switch", title: "Switches?", multiple: true, required: true
+		}
 
-        section("Number of active lights at any given time") { 
-            input name: "number_of_active_lights", type: "number", title: "Active?" 
-        }
+		section("Number of active lights at any given time") {
+			input name: "number_of_active_lights", type: "number", title: "Active?"
+		}
 
 		section("How many seconds to wait after turning all lights off, before starting the next cycle.") {
-        	input name: "cycle_delay", type: "number", title: "Seconds?"
-        }
+			input name: "cycle_delay", type: "number", title: "Seconds?"
+		}
 
-        section("Minimum number of minutes for each cycle") { 
-            input name: "frequency_minutes", type: "number", title: "Minutes?" 
-        }
-        
-        section {
-        	href name: "moreOptions", title: "More Options", page: "moreOptions", state: hasMoreOptions()
-        }
+		section("Minimum number of minutes for each cycle") {
+			input name: "frequency_minutes", type: "number", title: "Minutes?"
+		}
+		
+		section {
+			href name: "moreOptions", title: "More Options", page: "moreOptions", state: hasMoreOptions()
+		}
 
 	}
 }
 
 def moreOptions() {
 
-    def daysMap = [
-        "Monday", 
-        "Tuesday", 
-        "Wednesday", 
-        "Thursday", 
-        "Friday", 
-        "Saturday", 
-        "Sunday"]
+	def daysMap = [
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+		"Sunday"]
 
-	dynamicPage(name: "moreOptions", title: "More", nextPage: "mainPage") {    
-        section("Maximum number of minutes for each cycle (if not specified, will use minimum)") { 
-	    	paragraph "If you specify a both a minimum and a maximum number of minutes for a cycle, then a random number of minutes between the two will be chosen."
-			input name: "frequency_minutes_end", type: "number", title: "Minutes?", required: false 
-        }
+	dynamicPage(name: "moreOptions", title: "More", nextPage: "mainPage") {
+		section("Maximum number of minutes for each cycle (if not specified, will use minimum)") {
+			paragraph "If you specify a both a minimum and a maximum number of minutes for a cycle, then a random number of minutes between the two will be chosen."
+			input name: "frequency_minutes_end", type: "number", title: "Minutes?", required: false
+		}
 
-        section("How many seconds to pause between turning off each light") { 
-            input name: "light_off_delay", type: "number", title: "Seconds?", required: false 
-        }
+		section("How many seconds to pause between turning off each light") {
+			input name: "light_off_delay", type: "number", title: "Seconds?", required: false
+		}
 
-        section("How many seconds to pause between turning on each light") { 
-            input name: "light_on_delay", type: "number", title: "Seconds?", required: false 
-        }
+		section("How many seconds to pause between turning on each light") {
+			input name: "light_on_delay", type: "number", title: "Seconds?", required: false
+		}
 
-        section("People") { 
-            input name: "people", type: "capability.presenceSensor", title: "If these people are home do not change light status", required: false, multiple: true
-        }
+		section("People") {
+			input name: "people", type: "capability.presenceSensor", title: "If these people are home do not change light status", required: false, multiple: true
+		}
 
 		section("Day and/or Time") {
-         	href "timeIntervalInputStart", title: "Only during a certain time", description: getTimeLabel(starting, ending), state: getInputState(starting || ending)
-            input name: "days", type: "enum", title: "Only on certain days of the week", description: "Days?", multiple: true, required: false, options: daysMap
-        }
+			 href "timeIntervalInputStart", title: "Only during a certain time", description: getTimeLabel(starting, ending), state: getInputState(starting || ending)
+			input name: "days", type: "enum", title: "Only on certain days of the week", description: "Days?", multiple: true, required: false, options: daysMap
+		}
 
 		section() {
-            label title:"Assign a name", required:false
-            input name: "falseAlarmThreshold", type: "decimal", title: "Delay to start simulator... (defaults to 2 min)", description: "Minutes?", required: false       
-    	}
-    }
+			label title:"Assign a name", required:false
+			input name: "falseAlarmThreshold", type: "decimal", title: "Delay to start simulator... (defaults to 2 min)", description: "Minutes?", required: false
+		}
+	}
 }
 
 def timeIntervalInputStart() {
 	dynamicPage(name: "timeIntervalInputStart", title: "Only during a certain time", nextPage: "timeIntervalInputEnd", install: false) {
-	    section {
-            input "starting", "time", title: "Starting", required: false
-        }
-    }
+		section {
+			input "starting", "time", title: "Starting", required: false
+		}
+	}
 }
 
 def timeIntervalInputEnd() {
 	dynamicPage(name: "timeIntervalInputEnd", title: "Only during a certain time", nextPage: "moreOptions") {
-	    section {
-            input "ending", "time", title: "Ending (required if starting time is specified)", required: starting
-        }
-    }
+		section {
+			input "ending", "time", title: "Ending (required if starting time is specified)", required: starting
+		}
+	}
 }
 
 def hasMoreOptions() {
@@ -145,7 +145,7 @@ def updated() {
 def initialize(){
 	if (newMode != null) {
 		subscribe(location, modeChangeHandler)
-        runApp(location.mode)
+		runApp(location.mode)
 	}
 }
 
@@ -155,23 +155,23 @@ def modeChangeHandler(evt) {
 
 def runApp(theMode) {
 	log.debug "Mode change to: ${theMode}"
-    
+	
 	// Have to handle when they select one mode or multiple
 	if (newMode.any{ it == theMode } || newMode == theMode) {
 		def delay = (falseAlarmThreshold ?: 2) * 60
-        def startTime = calculateStartTimeFromInput()
-        
-        //calculateStartTimeFromInput COULD return a value that is smaller than now(), however
-        //if it did, the resulting subtraction would be negative but so close to 
-        //zero that it wouldn't really matter for this comparison.
-        if((startTime.time - now()) <= (delay * 1000)) {
-        	log.debug("Adding $delay second delay")
+		def startTime = calculateStartTimeFromInput()
+		
+		//calculateStartTimeFromInput COULD return a value that is smaller than now(), however
+		//if it did, the resulting subtraction would be negative but so close to
+		//zero that it wouldn't really matter for this comparison.
+		if((startTime.time - now()) <= (delay * 1000)) {
+			log.debug("Adding $delay second delay")
 			delay += startTime[Calendar.SECOND]
-            startTime[Calendar.SECOND] = delay
-        }
-        
-        log.debug("Will run at ${startTime}")
-        runOnce(startTime, scheduleCheck)
+			startTime[Calendar.SECOND] = delay
+		}
+		
+		log.debug("Will run at ${startTime}")
+		runOnce(startTime, scheduleCheck)
 	}
 	else {
 		//don't turn off lights if anyone is home
@@ -190,50 +190,50 @@ def runApp(theMode) {
 def calculateStartTimeFromInput() {
 	def now = new Date()
 	def nextFire = new Date(now.time)
-    
+	
 	if(starting) {
-    	def startingToday = timeToday(starting, location.timeZone)
-        def endingToday = timeTodayAfter(startingToday, ending, location.timeZone)
-    	if(!timeOfDayIsBetween(startingToday, endingToday, now, location.timeZone)) {
-            nextFire = timeTodayAfter(now, starting, location.timeZone)
-        }
-    }
-        
+		def startingToday = timeToday(starting, location.timeZone)
+		def endingToday = timeTodayAfter(startingToday, ending, location.timeZone)
+		if(!timeOfDayIsBetween(startingToday, endingToday, now, location.timeZone)) {
+			nextFire = timeTodayAfter(now, starting, location.timeZone)
+		}
+	}
+		
 	if(days) {
-    	def oldNextFire = nextFire
-    	nextFire = nextOccurrence(nextFire, days)
-        
-        if((oldNextFire - nextFire) && !starting) {
-        	//if there was no starting time, then clearTime, which sets to midnight
-            nextFire = nextFire.clearTime()
-        }
-    }
-    
-    log.debug("Next fire date is: ${nextFire}")
-    return nextFire;
+		def oldNextFire = nextFire
+		nextFire = nextOccurrence(nextFire, days)
+		
+		if((oldNextFire - nextFire) && !starting) {
+			//if there was no starting time, then clearTime, which sets to midnight
+			nextFire = nextFire.clearTime()
+		}
+	}
+	
+	log.debug("Next fire date is: ${nextFire}")
+	return nextFire;
 }
 
 def nextOccurrence(time, daysOfWeek) {
 	def daysMap = [
-    	"Monday": 2,
-        "Tuesday": 3,
-        "Wednesday": 4,
-        "Thursday": 5,
-        "Friday": 6,
-        "Saturday": 7,
-        "Sunday": 1 ]
-        
+		"Monday": 2,
+		"Tuesday": 3,
+		"Wednesday": 4,
+		"Thursday": 5,
+		"Friday": 6,
+		"Saturday": 7,
+		"Sunday": 1 ]
+		
 	def daysInt = daysOfWeek instanceof String ? [ daysMap[daysOfWeek] ] : daysOfWeek.collect { daysMap[it] }.sort()
-    
-    def dayOfWeek = time[Calendar.DAY_OF_WEEK]
-    def nextDay = daysInt.find { day -> day >= dayOfWeek } ?: daysInt.first()
+	
+	def dayOfWeek = time[Calendar.DAY_OF_WEEK]
+	def nextDay = daysInt.find { day -> day >= dayOfWeek } ?: daysInt.first()
 
 	//calculate the number of days between the two days of the week
-    def daysBetween = (nextDay < dayOfWeek ? 7 : 0) + nextDay - dayOfWeek
-    
-    //add to DAY_OF_MONTH. Will cause 'time' to roll if necessary
-	time[Calendar.DAY_OF_MONTH] = time[Calendar.DAY_OF_MONTH] + daysBetween        
-    time
+	def daysBetween = (nextDay < dayOfWeek ? 7 : 0) + nextDay - dayOfWeek
+	
+	//add to DAY_OF_MONTH. Will cause 'time' to roll if necessary
+	time[Calendar.DAY_OF_MONTH] = time[Calendar.DAY_OF_MONTH] + daysBetween
+	time
 }
 
 // We want to turn off all the lights
@@ -241,7 +241,7 @@ def nextOccurrence(time, daysOfWeek) {
 // Then run it again when the frequency demands it
 def scheduleCheck(evt) {
 	log.debug("Running scheduleCheck")
-    
+	
 	if(allOk){
 		log.debug("Running")
 		turnOn(evt)
@@ -250,11 +250,11 @@ def scheduleCheck(evt) {
 	else if (modeOk) {
 		log.debug("mode OK.  Running again")
 		switches.off()
-        runOnce(calculateStartTimeFromInput(), scheduleCheck)
+		runOnce(calculateStartTimeFromInput(), scheduleCheck)
 	}
 	//if none is ok turn off frequency check and turn off lights.
 	else if(people){
-    	//necessary?
+		//necessary?
 		//don't turn off lights if anyone is home
 		if(anyoneIsHome()){
 			log.debug("Stopping Check for Light")
@@ -268,38 +268,38 @@ def scheduleCheck(evt) {
 
 def turnOn(evt, theSwitches = null, numOn = null) {
 	theSwitches = theSwitches ?: allOff.clone()
-	numOn = (numOn ?: allOn.size()) 
+	numOn = (numOn ?: allOn.size())
 
 	log.debug("$numOn lights are on. I have ${theSwitches.size()} available switches")
 	log.debug("Turning on lights ${light_on_delay ? 'slowly' : 'quicky'}")
 
-    if (numOn < number_of_active_lights) {
-        def theSwitch = getRandomN(1, theSwitches, !light_on_delay)
-        log.debug("Turning on ${theSwitch.label}")
-        theSwitch.on()
-        
-    	light_on_delay ? runIn(light_on_delay, turnOnHandler) : turnOn(evt, theSwitches, numOn + 1)
-    } else {
-        runIn(getNextRunTime(), turnOff)
-    }
+	if (numOn < number_of_active_lights) {
+		def theSwitch = getRandomN(1, theSwitches, !light_on_delay)
+		log.debug("Turning on ${theSwitch.label}")
+		theSwitch.on()
+		
+		light_on_delay ? runIn(light_on_delay, turnOnHandler) : turnOn(evt, theSwitches, numOn + 1)
+	} else {
+		runIn(getNextRunTime(), turnOff)
+	}
 }
 
 def turnOff(evt) {
 	if(!light_off_delay) {
-    	log.debug("Turning off all lights quickly")
-    	switches.off()
-        runIn(cycle_delay, scheduleCheck)
-    } else {
-    	log.debug("Turning off lights slowly")
-        
-        if (allOn.size() > 0) {
-        	log.debug("Turning off ${allOn.first().label}")
-	        allOn.first().off()
-            runIn(light_off_delay, turnOff)
-        } else {
-            runIn(cycle_delay, scheduleCheck)
-        }
-    }
+		log.debug("Turning off all lights quickly")
+		switches.off()
+		runIn(cycle_delay, scheduleCheck)
+	} else {
+		log.debug("Turning off lights slowly")
+		
+		if (allOn.size() > 0) {
+			log.debug("Turning off ${allOn.first().label}")
+			allOn.first().off()
+			runIn(light_off_delay, turnOff)
+		} else {
+			runIn(cycle_delay, scheduleCheck)
+		}
+	}
 }
 
 def turnOnHandler(evt) {
@@ -307,9 +307,9 @@ def turnOnHandler(evt) {
 }
 
 def getNextRunTime() {
-    def freq = getRandomBetween(frequency_minutes, frequency_minutes_end) * 60
-    log.debug("Next cycle time: ${freq}")
-    return freq
+	def freq = getRandomBetween(frequency_minutes, frequency_minutes_end) * 60
+	log.debug("Next cycle time: ${freq}")
+	return freq
 }
 
 def switchesWithState(theState) {
@@ -327,12 +327,12 @@ def getAllOff() {
 def getRandomN(num, theList, remove = false) {
 	def r = new Random()
 	def listCopy = remove ? theList : theList.clone()
-    def subset = []
-    
-    num = Math.min(listCopy.size(), num)    
+	def subset = []
+	
+	num = Math.min(listCopy.size(), num)
 	num.times { subset << listCopy.remove(r.nextInt(listCopy.size())) }
-    
-    subset.size() == 1 ? subset.first() : subset
+	
+	subset.size() == 1 ? subset.first() : subset
 }
 
 def getRandomBetween(start, end) {
@@ -368,14 +368,14 @@ private getDaysOk() {
 }
 
 private getTimeOk() {
-    def result = true
-    if (starting && ending) {
-        def todayStart = timeToday(starting, location.timeZone)
-        def todayEnd = timeTodayAfter(todayStart, ending, location.timeZone)
-        def now = new Date()
-        result = timeOfDayIsBetween(todayStart, todayEnd, now, location.timeZone)
-    }
-    return result
+	def result = true
+	if (starting && ending) {
+		def todayStart = timeToday(starting, location.timeZone)
+		def todayEnd = timeTodayAfter(todayStart, ending, location.timeZone)
+		def now = new Date()
+		result = timeOfDayIsBetween(todayStart, todayEnd, now, location.timeZone)
+	}
+	return result
 }
 
 def getTimeLabel(starting, ending){
@@ -417,4 +417,3 @@ private anyoneIsHome() {
 
 	return result
 }
-
